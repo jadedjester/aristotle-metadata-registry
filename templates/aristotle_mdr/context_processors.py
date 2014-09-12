84b9838cf4498bebaf14c2d7ee0779c87c442de0
theodore.therone@gmail.com <aristotle@ip-10-120-219-139.ec2.internal>
2014-09-12 06:42:58 +0000

    remove cruft

--- templates/aristotle_mdr/context_processors.py
+++
@@ -1,5 +0,0 @@
-
-# This allows us to pass the Aristotle settigns through to the final rendered page
-def settings(request):
-    from django.conf import settings
-    return {"config": getattr(settings, 'ARISTOTLE_SETTINGS', {})}
