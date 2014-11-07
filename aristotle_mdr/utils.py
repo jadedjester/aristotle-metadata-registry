e0ab11916f9290447f67357b4aaf8cbcccf9d8cc
theodore.therone@gmail.com <aristotle@ip-10-144-242-81.ec2.internal>
2014-11-07 02:34:02 +0000

    add docs

--- aristotle_mdr/utils.py
+++ aristotle_mdr/utils.py
@@ -1,6 +1,11 @@
 from django.forms import model_to_dict
 
 def concept_to_dict(obj):
+    """
+    A replacement for the ```django.form.model_to_dict`` that includes additional
+    ``ManyToManyFields``, but removes certain concept fields.
+    """
+
     excluded_fields='readyToReview version workgroup pk id supersedes superseded_by'.split()
     concept_dict = model_to_dict(obj,
         fields=[field.name for field in obj._meta.fields if field.name not in excluded_fields],
@@ -9,6 +14,11 @@ def concept_to_dict(obj):
     return concept_dict
 
 def concept_to_clone_dict(obj):
+    """
+    An extension of ``aristotle_mdr.utils.concept_to_dict`` that adds a 'clone'
+    suffix to the name when cloning an item.
+    """
+
     from django.utils.translation import ugettext # Do at run time because reasons
     clone_dict = concept_to_dict(obj)
     clone_dict['name'] = clone_dict['name'] + ugettext(u" (clone)")
