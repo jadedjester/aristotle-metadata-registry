59f21c716ffde7c58a0aaa384cdd6f7f6d62751e
theodore.therone@gmail.com <aristotle@ip-10-144-242-81.ec2.internal>
2014-11-20 06:08:06 +0000

    force migrate

--- aristotle_mdr/tests/settings.py
+++ aristotle_mdr/tests/settings.py
@@ -2,7 +2,7 @@ import os
 from aristotle_mdr.required_settings import *
 
 SECRET_KEY = 'inara+vtkprm7@0(fsc$+grbz9-s+tmo9d)e#k(9uf8m281&$7xhdkjr'
-#SOUTH_TESTS_MIGRATE = False
+SOUTH_TESTS_MIGRATE = True
 #SKIP_SOUTH_TESTS = True
 
 DATABASES = {
